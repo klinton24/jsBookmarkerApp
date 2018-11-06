cf0ef530f1e5a4ff7a3a5bf2f14482a7fd6c2e1e
K Smith <klinton.jp@gmail.com>
2018-11-06 15:16:24 -0800

    Finished html, style and js, tested and working right.

--- js/bookmarker.js
+++ js/bookmarker.js
@@ -1,4 +1,104 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-function saveBookmark(){
+function saveBookmark(e) {
+
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    if(!validateForm(siteName, siteUrl)){
+        return false;
+    }
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    // test if array is there
+    if (localStorage.getItem('bookmarks') === null) {
+        // init array
+        var bookmarks = [];
+        // add to array
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // Get bookmarks from localStorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // add bookmark to array
+        bookmarks.push(bookmark);
+        // re-set back to localstorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
+    // Clear form
+    document.getElementById('myForm').reset();
+
+    //re-fetch bookmarks to fix list
+    fetchBookmarks();
+
+    //prevent form from submitting/clearing
+    e.preventDefault();
+
+};
+
+function deleteBookmark(url) {
+    // get bookmarks rom local storage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // loop through bookmarks
+    for (var i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
+            bookmarks.splice(i, 1);
+        }
+    }
+    // re-set back to localstorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    //re-fetch bookmarks to fix list
+    fetchBookmarks();
+}
+
+// Fetch bookmarks
+function fetchBookmarks(){
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+  
+    // Build output
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++){
+      var name = bookmarks[i].name;
+      var url = bookmarks[i].url;
+  
+      bookmarksResults.innerHTML += '<div class="well">'+
+                                    '<h3>'+name+
+                                    ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+                                    ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                    '</h3>'+
+                                    '</div>';
+    }
+  }
+// Validate Form
+function validateForm(siteName, siteUrl) {
+    if (!siteName || !siteUrl) {
+        alert('Please fill in the form');
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if (!siteUrl.match(regex)) {
+        alert('Please use a valid URL');
+        return false;
+    }
+
+    return true;
+}
+
+function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
 }
